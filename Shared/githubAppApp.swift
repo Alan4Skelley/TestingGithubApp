c5f10bcaaff09838d26bb5ec93dfef4b694f8e86
Alan4Skelley <alan4skelley@gmail.com>
2021-02-24 13:40:44 -0800

    Initial Commit

---
+++ Shared/githubAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubAppApp.swift
+//  Shared
+//
+//  Created by Alan jr. Skelley on 2/24/21.
+//
+
+import SwiftUI
+
+@main
+struct githubAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
